591c8e334c289b66bac001f7876db6979a3b58e3
gcrespo <gcrespo1705@gmail.com>
2025-11-24 23:03:06 -0400

    created wifi list functions

---
+++ src/WifiList.c
@@ -0,0 +1,58 @@
+#include "WifiList.h"
+#include "WifiData.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int dataId = 0;
+
+// Inicializo mi lista de redes para tener 1 con valor 0;
+void wifiListConstructor(WifiList *networks) {
+    networks->size = 0;
+    networks->data = NULL;
+    networks->capacity = 10;
+}
+
+// Creo un elemento, donde se va a tener mis datos de la red
+WifiData createData(char *ssid, char signal[]) {
+    WifiData data;
+    data.id = dataId;
+    data.ssid = ssid;
+    strcpy(data.signal, signal);
+    dataId++;
+    return data;
+}
+
+// Agrego un nuevo elemento a mi arreglo dinamico
+void addNetwork(WifiList *wifiList, WifiData wifiData) {
+    // Verifico si el tamaño actual es igual al de lo que puede aguantar
+    if (wifiList->size == wifiList->capacity) {
+        // Coloco que mi nueva capacidad sea 50% mayor al de mi tamaño actual
+        int newCapacity = wifiList->size * 1.5;
+        // Creo memoria para varios datos nuevos,
+        WifiData *newData = malloc(newCapacity * sizeof(WifiData));
+        if (!newData) {
+            perror("Malloc failed");
+            exit(EXIT_FAILURE);
+        }
+        // Se le dice que la memoria reservada es de la lista de wifi
+        wifiList->data = newData;
+        // Se expande la capacidad
+        wifiList->capacity = newCapacity;
+    }
+    // Se le agrega la data al final del arreglo
+    wifiList->data[wifiList->size] = wifiData;
+    wifiList->size++;
+}
+
+WifiData getData(WifiList *wifiList, int dataId) {
+    return wifiList->data[dataId];
+}
+
+void freeWifiList(WifiList *wifiList) {
+    free(wifiList->data);
+    wifiList->data = NULL;
+    wifiList->capacity = 0;
+    wifiList->size = 0;
+}
