ed87a2b387c209f7ea057487853803ce31c1b928
gcrespo <gcrespo1705@gmail.com>
2025-11-26 13:59:10 -0400

    fixed to octave ansii and added text invertion

--- include/tui.h
+++ include/tui.h
@@ -2,32 +2,32 @@
 #define HEADER_TUI
 
 /***************** COLOR SEGMENT **************************/
-#define FONT_RESET "\1xB[0m"
-#define FONT_RED "\1xB[1;31m"
-#define FONT_GRN "\1xB[1;32m"
-#define FONT_YEL "\1xB[1;33m"
-#define FONT_BLU "\1xB[1;34m"
-#define FONT_MAG "\1xB[1;35m"
-#define FONT_CYN "\1xB[1;36m"
-#define FONT_WHT "\1xB[1;37m"
+#define FONT_RESET "\033[0m"
+#define FONT_RED "\033[1;31m"
+#define FONT_GRN "\033[1;32m"
+#define FONT_YEL "\033[1;33m"
+#define FONT_BLU "\033[1;34m"
+#define FONT_MAG "\033[1;35m"
+#define FONT_CYN "\033[1;36m"
+#define FONT_WHT "\033[1;37m"
 
-#define FONT_RESET "\1xB[0m"
-#define FONT_B_RED "\1xB[0;31m"
-#define FONT_B_GRN "\1xB[0;32m"
-#define FONT_B_YEL "\1xB[0;33m"
-#define FONT_B_BLU "\1xB[0;34m"
-#define FONT_B_MAG "\1xB[0;35m"
-#define FONT_B_CYN "\1xB[0;36m"
-#define FONT_B_WHT "\1xB[0;37m"
 
-#define BG_RESET "\1xB[40m"
-#define BG_RED "\1xB[41m"
-#define BG_GRN "\1xB[42m"
-#define BG_YEL "\1xB[43m"
-#define BG_BLU "\1xB[44m"
-#define BG_MAG "\1xB[45m"
-#define BG_CYN "\1xB[46m"
-#define BG_WHT "\1xB[47m"
+#define FONT_B_RED "\033[0;31m"
+#define FONT_B_GRN "\033[0;32m"
+#define FONT_B_YEL "\033[0;33m"
+#define FONT_B_BLU "\033[0;34m"
+#define FONT_B_MAG "\033[0;35m"
+#define FONT_B_CYN "\033[0;36m"
+#define FONT_B_WHT "\033[0;37m"
+
+#define BG_RESET "\033[40m"
+#define BG_RED "\033[41m"
+#define BG_GRN "\033[42m"
+#define BG_YEL "\033[43m"
+#define BG_BLU "\033[44m"
+#define BG_MAG "\033[45m"
+#define BG_CYN "\033[46m"
+#define BG_WHT "\033[47m"
 
 /***************** END COLOR SEGMENT **************************/
 
@@ -39,9 +39,20 @@
 #define MV_CURSOR "\033[0;0H"
 /***************** END CURSOR SEGMENT **************************/
 
+/***************** KEYS SEGMENT **************************/
+#define KEY_UP "\033[A"
+#define KEY_DOWN "\033[B"
+#define KEY_RIGHT "\033[C"
+#define KEY_LEFT "\033[D"
+#define KEY_ENTER '\n'
+#define KEY_QUIT 'q'
+
+/***************** END KEYS SEGMENT **************************/
+
+
 void configureTerminal();
 void resetTerminal();
 int readInput();
-int readkey();
+int readkey(char *buffer, int key);
 
 #endif
