5575be2497c7e4c907b1151abafc2b86bb776008
gcrespo <gcrespo1705@gmail.com>
2025-12-01 15:42:14 -0400

    added connect to network and check if the input is dangerous

--- src/WifiController.c
+++ src/WifiController.c
@@ -2,6 +2,7 @@
 #include "WifiData.h"
 #include "WifiList.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void printAvailableNetworks() {
@@ -46,4 +47,34 @@ void fillWifiList(WifiList *wifiList) {
     }
 
     pclose(fp);
+}
+
+bool isDangerousInput(const char *input) {
+    const char *danger = "\'";
+
+    if (strpbrk(input, danger) != NULL) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int connectNetwork(char *ssid, char *password) {
+    if (!isDangerousInput(ssid) && !isDangerousInput(password)) {
+        char cleanNetworkCommand[150] = "nmcli connection delete ";
+        char command[150] = "nmcli device wifi connect ";
+        strcat(command, ssid);
+        strcat(command, " password ");
+        strcat(command, password);
+        strcat(command, " > /dev/null 2>&1");
+        // Para limpiar la red
+        strcat(cleanNetworkCommand, ssid);
+        strcat(cleanNetworkCommand, " > /dev/null 2>&1");
+        int status;
+        system(cleanNetworkCommand);
+        status = system(command);
+        return status;
+    } else {
+        return -1;
+    }
 }
